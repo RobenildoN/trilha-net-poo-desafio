fc2c7c9dee5718b2f5689809f9009b375b7308f7
Robenildo <robenildo_noberto@yahoo.com.br>
2025-07-24 17:21:50 -0300

    Implementando ações pedidas

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main()
+    {
+        Smartphone iphone = new Iphone("99999-9999", "iPhone 14", "123456789012345", 128);
+        Smartphone nokia = new Nokia("88888-8888", "Nokia Tijolão", "987654321098765", 32);
+
+        Console.WriteLine("Testando iPhone:");
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+
+        Console.WriteLine();
+
+        Console.WriteLine("Testando Nokia:");
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("Snake");
+    }
+}
